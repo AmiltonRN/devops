0bd02adbcedadfacee89af8b07ae8effa0831d0e
Amilton Nunes <amilton.r.n@hotmail.com>
2023-04-04 01:09:44 -0300

    Correções no dto e save do resource

--- src/main/java/br/edu/infnet/devops/dto/UserDto.java
+++ src/main/java/br/edu/infnet/devops/dto/UserDto.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-
-    private Long id;
     private String name;
     private Integer age;
 }
--- src/main/java/br/edu/infnet/devops/resource/UserResource.java
+++ src/main/java/br/edu/infnet/devops/resource/UserResource.java
@@ -3,6 +3,8 @@ package br.edu.infnet.devops.resource;
 import br.edu.infnet.devops.dto.UserDto;
 import br.edu.infnet.devops.model.User;
 import br.edu.infnet.devops.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,22 +15,26 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserResource {
 
+    final static Logger logger = LoggerFactory.getLogger(UserResource.class);
     @Autowired
     UserRepository userRepository;
 
     @GetMapping(produces = "application/json")
     public List<User> findAll() {
+        logger.info("findAll");
         return userRepository.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
     public User findById(@PathVariable("id") Long id) {
+        logger.info("findById");
         return userRepository.findById(id).orElseThrow();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public User save(UserDto userRequest) {
+    public User save(@RequestBody UserDto userRequest) {
+        logger.info("save");
         User user = new User();
         user.setAge(userRequest.getAge());
         user.setName(userRequest.getName());
@@ -38,6 +44,7 @@ public class UserResource {
     @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public User update(@PathVariable("id") Long id, @RequestBody UserDto userData) {
+        logger.info("update");
         User user = userRepository.findById(id).orElseThrow();
         user.setAge(userData.getAge());
         user.setName(userData.getName());
@@ -48,6 +55,7 @@ public class UserResource {
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable("id") Long id) {
+        logger.info("delete");
         userRepository.deleteById(id);
     }
 
