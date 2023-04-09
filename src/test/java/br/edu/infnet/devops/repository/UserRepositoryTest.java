779974fe5d39cdb5a3407c936f5c95440068e92c
Amilton Nunes <amilton.r.n@hotmail.com>
2023-04-08 21:56:36 -0300

    Correção na execução dos testes.

--- src/test/java/br/edu/infnet/devops/repository/UserRepositoryTest.java
+++ src/test/java/br/edu/infnet/devops/repository/UserRepositoryTest.java
@@ -10,12 +10,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class UserRepositoryTest {
     @Mock
     private UserRepository userRepository;
